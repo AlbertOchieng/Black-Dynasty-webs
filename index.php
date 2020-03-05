60808b8972a4cd21c1fd6e1e291aa52094d572df
albertzkiki <albertzkiki@gmail.com>
2020-03-05 12:43:01 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,436 @@
+<!DOCTYPE html>
+<html lang="en">
+  
+<!-- Mirrored from blackdynasty.000webhostapp.com/ by HTTrack Website Copier/3.x [XR&CO'2014], Thu, 28 Sep 2017 22:25:36 GMT -->
+<!-- Added by HTTrack --><meta http-equiv="content-type" content="text/html;charset=UTF-8" /><!-- /Added by HTTrack -->
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Black Dynasty</title>
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css"  rel="stylesheet">
+	  <link href="font-awesome-4.7.0/css/font-awesome.css" rel="stylesheet">
+	<link href="css/style.css" rel="stylesheet">
+	  <link href="css/animate.min.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+	<!--NAVIGATION-->
+	
+ 
+	
+	<div id="myNavbar" class="navbar navbar-default navbar-fixed-top" role="navigation">
+		<div class="container">
+			<div class="navbar-header">
+				<button class="navbar-toggle" type="button" data-toggle="collapse" data-target=".navbar-collapse">
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				</button>
+				
+				<a href="#" class="navbar-brand">Media</a>
+			</div>
+			<div class="navbar-collapse collapse">
+				<ul class="nav navbar-nav navbar-right">
+					<li><a href="#header">Header</a></li>
+					<li><a href="#services">Services</a></li>
+					<li><a href="#pricing">Pricing</a></li>
+					<li><a href="#team">Team</a></li>
+					<li><a href="#clients">Client</a></li>
+					<li><a href="#contact">Contact</a></li>
+				</ul>
+			</div>
+
+		</div>
+	</div>
+	
+	<!--END NAVIGATION-->
+	
+	<div id="header" class="header">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-6 col-md-6 wow fadeInUp" data-wow-delay="1s">
+				<img src="images/boom.png" alt="">
+				</div>
+				<div class="col-lg-6 col-md-6">
+					</br>
+					</br>
+					</br>
+					<h1 class="wow fadeInDown" data-wow-delay="1s">Black Dynasty</h1>
+					<p class="wow bounceInRight">	BD entertainment has been in the industry for over 10years now,
+						we host special event entertainment where we pride ourselvers 
+						on proffessional service,
+						flexibility,attention to detail client satisfaction and top quality entertainment.<br>
+						we belive that every event no matter the size,is an opportunity for excellence...<br>
+						Let us be a part of making your next event pure..GENIUS!!!
+					</p>
+						<br>
+						<br>
+						<br>
+					<p class="wow fadeInDown" data-wow-delay="1.5s">
+						<b>Listen to our mixtape!!</b>
+					</p>
+					<audio controls class="wow fadeInDown" data-wow-delay="2s">
+						<source src="images/djalbert.mp3" type="audio/ogg">
+						<source src="images/djalbert.mp3" type="audio/mpeg">
+						Your browser does not support the audio element.
+					</audio>
+
+					<br>
+					<br>
+					<br>
+
+					
+
+
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<!--END OF  HEADER-->
+	
+	<!--SERVICES-->
+	
+	<div id="services" class="services">
+		<div class="container">
+			<h2 class="wow fadeInUp">SERVICES</h2>
+			<p class="wow fadeInDown" data-wow-delay="0.8s">Black Dynasty Enetertainment organises all kinds of events:wedding parties,concerts,
+				official events,birthday parties etc</br>
+				Apart from hosting major events, we also offer the following Skills
+				at our Empire:Deejaying,GraphicDesign and Modern Dance choreography</p>
+				<div class="row">
+					<div class="col-lg-3 col-md-3 wow flipInX " data-wow-delay="2.6s">
+				
+					<h4>Event Planning</h4>
+					<p>we offer the best and quality events with High Defination sound system and a well 
+					enhanced security team</p>
+					<img src="images/dj.png" alt="">
+					</div>
+					
+					<div class="col-lg-3 col-md-3 col-md-3  wow flipInX " data-wow-delay="2.2s">
+			
+					<h4>DeeJaying</h4>
+					<p>We Offer The following Packages in DeeJaying</p>
+					
+					<ul class="list-group">
+					<li class="list-group-item">Scratching</li>
+					<li class="list-group-item">Syncing</li>
+					<li class="list-group-item">BPM's</li>
+					</ul>
+					</div>
+					
+					<div class="col-lg-3 col-md-3 col-md-3 wow flipInY" data-wow-delay="1.7s" >
+					<h4>GraphicDesign</h4>
+					<p>We Offer The following Packages in Graphics</p>
+					<ul class="list-group">
+					<li class="list-group-item">Logos and cooperate identity</li>
+					<li class="list-group-item">portfolio Design</li>
+					<li class="list-group-item">Game Design</li>
+					</ul>
+					</div>
+					
+					<div class="col-lg-3 col-md-3 col-md-3  wow flipInY" data-wow-delay="1.3s" >
+					<h4>Modern Dance Choreography</h4>
+					<p>Come and Learn the best and coolest dance Moves</p>
+					<img src="images/dan.png" alt="">
+					</div>
+				</div>
+		</div>
+	</div>
+	<!--END OF SERVICES-->
+	
+	<!--PRICING-->
+	<div id="pricing" class="pricing">
+		<div class="container">
+			<div class="row">
+				<h2 class="wow fadeInUp">PRICING</h2>
+				<p class="wow fadeInDown" data-wow-delay="0.8s">We provide comprehensive Audio Visual(AV),sound and lighting and Entertainment production
+					services for any event with effortless customer services and flawless execution.
+					Our prices are very affortable because there is something for everyone</p>
+					<div class="col-lg-3 col-md-3 col-md-3  wow rollIn" data-wow-delay="2.5s">
+						<div class="packages">
+							<h1>Express Package</h1>
+							<h4>20,000/= Ksh</h4>
+							<b>4 Hours</b>
+							
+							<hr>
+							<li>Dj Services</li>
+							<li>Digital sound system</li>
+							<li>Wireless Microphone</li>
+							<li>Customizable Music selection</li>
+							<li>Free set up and sound check</li>
+							<p>Not available on Saturday and only available on selected events</p>
+							</br>
+							</br>
+							</br>
+							</br>
+							</br>
+							<button class="btn btn-success">Get Quote</button>
+						</div>
+					</div>
+					
+					<div class="col-lg-3 col-md-3 col-md-3 wow rotateInDownRight" data-wow-delay="2.1s">
+						<div class="packages">
+							<h1>Smart Package</h1>
+							<h4>35,000/= Ksh</h4>
+							<b>4 Hours</b>
+							<hr>
+							<li>Proffessional Dj/Mc</li>
+							<li>"Strate of the art"Digital sound system</li>
+							<li>Wireless Microphone</li>
+							<li>Extra Speakers(for cocktail Hour)</li>
+							<li>Customizable Music selection</li>
+							<li>Free set up and sound check</li>
+							<p>Not available on Sunday and only available on selected events</p>
+							</br>
+							
+							<button class="btn btn-success">Get Quote</button>
+						</div>
+					</div>
+					
+					<div class="col-lg-3 col-md-3 col-md-3 wow rotateInDownLeft" data-wow-delay="1.6s">
+						<div class="packages">
+							<h1>Brilliant Package</h1>
+							<h4>50,000/= Ksh</h4>
+							<b>5 Hours</b>
+							<hr>
+							<li>Proffessional Dj/Mc</li>
+							<li>"Strate of the art"Digital sound system</li>
+							<li>Wireless Microphone</li>
+							<li>Wedding Ceremony package</li>
+							<li>LED party lights or "up" Lights</li>
+							<li>Extra Speakers(for cocktail Hour)</li>
+							<li>Customizable Music selection</li>
+							<li>Free set up and sound check</li>
+							</br>
+							</br>
+							<button class="btn btn-success">Get Quote</button>
+						</div>
+					</div>
+					
+					<div class="col-lg-3 col-md-3 wow rotateInDownRight" data-wow-delay="1.2s">
+						<div class="packages">
+							<h1>Dynasty Package</h1>
+							<h4>70,000/= Ksh</h4>
+							<b>6 Hours</b>
+							<hr>
+							<li>Your Choice of Proffessional Dj/Mc</li>
+							<li>Vedio Recording</li>
+							<li>"Strate of the art"Digital sound system</li>
+							<li>Wireless Microphone</li>
+							<li>Wedding Ceremony package</li>
+							<li>LED party lights or "up" Lights</li>
+							<li>Extra Speakers(for cocktail Hour)</li>
+							<li>Customizable Music selection</li>
+							<li>Free set up and sound check</li>
+							<button class="btn btn-success">Get Quote</button>
+						</div>
+					</div>
+			</div>
+		</div>
+	</div>
+	
+	<!--End of pricing-->
+	
+	<!--Team-->
+	
+	<div class="team" id="team">
+		<div class="container">
+			<div class="row">
+				<h2 class="wow fadeInUp">MEET OUR TEAM</h2>
+				<p class="wow fadeInDown" data-wow-delay="0.4s">The following are our executive Leaders...The brilliant minds behind black dynasty Entertainment </p>
+				<div class="col-lg-3 col-md-3 wow rollIn" data-wow-delay="0.7s">
+					<img src="images/albert.png" class="img-circle" alt="">
+					<h4>AlbertzKiki</h4>
+					<b>CEO and Founder</b></br>
+					<b>Software Engineer</b>
+
+					<blockquote>
+						They Try to Imitate...but they cant swagger like us!!
+						Quote by albertzkiki
+					</blockquote>
+					<a href ="#"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+					<a href ="#"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+					<a href ="#"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+					<a href ="#"><i class="fa fa-whatsapp" aria-hidden="true"></i></a>
+
+				</div>
+				
+				<div class="col-lg-3 col-md-3 wow rollIn" data-wow-delay="1.1s">
+					<img src="images/kiki.png" class="img-circle" alt="">
+					<h4>Mc Colo</h4>
+					<b>Director</b></br>
+					<b>Sound Engineer</b>
+					<blockquote>
+						No woman...No party!!
+						Quote by Mc Colo
+					</blockquote>
+					<br>
+					<a href ="#"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+					<a href ="#"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+					<a href ="#"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+					<a href ="#"><i class="fa fa-whatsapp" aria-hidden="true"></i></a>
+				</div>
+				
+				<div class="col-lg-3 col-md-3 wow rollIn " data-wow-delay="1.6s">
+					<img src="images/asse.png" class="img-circle" alt="">
+					<h4>Greg</h4>
+					<b>Dance Guru</b></br>
+					<b>Choreographer</b>
+					<blockquote>
+						Dance is freedom...Dance is Life</br>
+						Quote by greg
+					</blockquote>
+					<a href ="#"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+					<a href ="#"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+					<a href ="#"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+					<a href ="#"><i class="fa fa-whatsapp" aria-hidden="true"></i></a>
+				</div>
+				
+				<div class="col-lg-3 col-md-3 wow rollIn "data-wow-delay="2s">
+					<img src="images/greg.png" class="img-circle" alt="">
+					<h4>Dj Frooze</h4>
+					<b>Main Dj</b></br>
+					<b>Sound Engineer</b>
+					<blockquote>
+						Can knock your socks off..without taking off your shoes!!
+						dj frooze
+					</blockquote>
+					<a href ="#"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+					<a href ="#"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+					<a href ="#"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+					<a href ="#"><i class="fa fa-whatsapp" aria-hidden="true"></i></a>
+				</div>
+			</div>
+		</div>
+	</div>
+
+ <!--End of Team-->
+
+
+ <!--Clients-->
+
+ <div class="clients" id="clients">
+	 <div class="container">
+		 <div class="row">
+			<h2 class="wow fadeInDown" data-wow-delay="0.5s">TRUSTED BY</h2>
+			 <div class="col-lg-4 col-lg-4 wow zoomInLeft" data-wow-delay="2s">
+				<li><img src="images/soen.png" alt=""></li>
+			 </div>
+			 <div class="col-lg-4 col-lg-4 wow zoomIn" data-wow-delay="0.7s">
+				 <li><img src="images/dark.png" alt=""></li>
+			 </div>
+			 <div class="col-lg-4 col-lg-4 wow zoomInRight" data-wow-delay="2s">
+				 <li><img src="images/killernet2.png" alt=""></li>
+			 </div>
+		 </div>
+	 </div>
+ </div>
+
+ <!--End of Clients-->
+
+
+
+ <!--Contact-->
+
+ <div class="contact" id="contact">
+	 <div class="container">
+		 <p class="row">
+			 <h2 class="wow fadeInUp" >Contact</h2>
+			 <p class="wow fadeInDown" data-wow-delay="0.4s">We would love to be the company you trust to participate in your special Event.</p>
+		 		<form method="post" class="form-group" action="http://blackdynasty.000webhostapp.com/index.php" autocomplete="off">
+		 			<div class="col-lg-6 col-md-6 ">
+
+					<div class="input-group input-group-lg wow fadeInUp" data-wow-delay="0.8s">
+						<span class="input-group-addon" id="sizing-addon1"><i class="fa fa-user" aria-hidden="true"></i></span>
+						<input type="text" class="form-control" required placeholder="User name" name="user name" aria-describedby="sizing-addon1">
+
+					</div>
+					<div class="input-group input-group-lg wow fadeInUp" data-wow-delay="1.2s">
+						<span class="input-group-addon" id="sizing-addon2"><i class="fa fa-envelope" aria-hidden="true"></i></span>
+						<input type="email" class="form-control" name="email" required placeholder="Email" aria-describedby="sizing-addon2">
+
+					</div>
+					<div class="input-group input-group-lg wow fadeInUp" data-wow-delay="1.6s">
+						<span class="input-group-addon" id="sizing-addon3"><i class="fa fa-phone" aria-hidden="true"></i></span>
+						<input type="number" class="form-control" name="phonenumber" required placeholder="PhoneNumber" aria-describedby="sizing-addon3">
+
+					</div>
+					</div>
+
+				 <div class="col-lg-6 col-md-6">
+					<div class="input-group wow fadeInUp" data-wow-delay="2s">
+						<textarea name="textarea" id="" cols="80" rows="6" class="form-control">
+						</textarea>
+					</div>
+					 <button class="btn btn-md wow fadeInRight" name="submit" data-wow-delay="2.5s">
+							Submit your message</button>
+
+				 </div>
+			</form>
+	 	</div>
+ 	</div>
+
+
+ <!--End of Contact-->
+
+ <!--Footer-->
+	
+ <div class="footer" id="footer">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-4 col-md-4">
+				<h4>Contact Us</h4>
+				<p><i class="fa fa-home" aria-hidden="true"></i>111 Main Street,Nairobi,222222</p>
+				<p><i class="fa fa-envelope" aria-hidden="true"></i>black_dynasty@gmail.com</p>
+				<p><i class="fa fa-globe" aria-hidden="true"></i>www.BlackSynasty.com</p>
+				<p><i class="fa fa-phone" aria-hidden="true"></i>+254 718635418</p>
+			</div>
+
+			<div class="col-lg-4 col-md-4">
+				<h4>About</h4>
+				<p><i class="fa fa-square-o" aria-hidden="true"></i>About Us</p>
+				<p><i class="fa fa-square-o" aria-hidden="true"></i>Privacy </p>
+				<p><i class="fa fa-square-o" aria-hidden="true"></i>Terms & Condition</p>
+
+			</div>
+
+			<div class="col-lg-4 col-md-4">
+				<h4>Stay in Touch With us</h4>
+				<i class=" social fa fa-facebook" aria-hidden="true"></i>
+				<i class=" social fa fa-twitter" aria-hidden="true"></i>
+				<i class=" social fa fa-instagram" aria-hidden="true"></i>
+				<i class=" social fa fa-youtube" aria-hidden="true"></i>
+				<i class=" social fa fa-github" aria-hidden="true"></i>
+				<br>
+				<input type="EMAIL" placeholder="Subscribe for updates">
+				<button class="btn btn-success">Subscribe</button>
+			</div>
+		</div>
+	</div>
+ </div>
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="../ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+
+ 	<script src="js/wow.min.js"></script>
+ 	<script>
+		new WOW().init();
+	</script>
+
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+  <div style='text-align: right;position: fixed;z-index:9999999;bottom: 0; width: 100%;cursor: pointer;line-height: 0;'><a title="Hosted on free web hosting 000webhost.com. Host your own website for FREE." target="_blank" href="https://www.000webhost.com/?utm_source=000webhostapp&amp;utm_campaign=000_logo&amp;utm_medium=website_blackdynasty&amp;utm_content=footer_img"><img src="../cloud.githubusercontent.com/assets/23024110/20663010/9968df22-b55e-11e6-941d-edbc894c2b78.png"  alt="www.000webhost.com"></a></div></body>
+
+<!-- Mirrored from blackdynasty.000webhostapp.com/ by HTTrack Website Copier/3.x [XR&CO'2014], Thu, 28 Sep 2017 22:28:23 GMT -->
+</html>
